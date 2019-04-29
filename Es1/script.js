2cde9f3ba12f43af3a4cd81fe8b5acc49954a839
AlessandroRossi643 <alessandrorossi643@gmail.com>
2019-04-29 20:36:55 +0200

    es 2 fatto

--- Es1/script.js
+++ Es1/script.js
@@ -1,11 +1,16 @@
 // Inserimento parola dall'utente
 var parola = prompt("Inserisci parola:");
 
-//Verifica della parola: Palindroma o no?!
+// Si divide la parola in tante lettere con SPLIT
 var divlettere = parola.split("");
+
+// Utilizzando il REVERSE, si analizza la parola partendo dal fondo
 var contrario = divlettere.reverse();
+
+// Si riunisce nuovamente le lettere in unica stringa con JOIN
 var palindroma = contrario.join("");
 
+// Verifica della parola: Palindroma o no?!
 if (parola === palindroma) {
   document.writeln("La parola da te inserita è palindroma: " + parola);
 } else {
